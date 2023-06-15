5c8386c799f05e022f661d67c04c8f2a67fe9bdf
Dean Attali <daattali@gmail.com>
2023-06-15 13:30:27 -0400

    add get_drives() function to allow Windows users to browse different drives; fixes #7

---
+++ R/get_drives.R
@@ -0,0 +1,57 @@
+#' Get the drives on the current machine (Windows only)
+#'
+#' On Windows machines, the standard drive is `C:/`, but there may be other drives.
+#' You can use this function to allow [`file_browser()`] to browse in other drives.
+#'
+#' @return On Windows, the names of available drives. On non-Windows, `NULL`.
+#' @examples
+#' if (interactive()) {
+#'
+#' ### Simple app that lets you browse each drive
+#'
+#' library(shiny)
+#'
+#' ui <- fluidPage(
+#'   selectInput("drive", "Select drive", get_drives()),
+#'   file_browser_ui("files")
+#' )
+#'
+#' server <- function(input, output, session) {
+#'   file_browser_server("files", path = reactive(input$drive))
+#' }
+#'
+#' shinyApp(ui, server)
+#'
+#' ##########################
+#'
+#' ### App that defaults to the home directory on C:/ drive
+#'
+#' library(shiny)
+#'
+#' ui <- fluidPage(
+#'   selectInput("drive", "Select drive", get_drives()),
+#'   file_browser_ui("files")
+#' )
+#'
+#' server <- function(input, output, session) {
+#'   path <- reactive({
+#'     if (input$drive == "C:/") {
+#'       "~"
+#'     } else {
+#'       input$drive
+#'     }
+#'   })
+#'   file_browser_server("files", path = path, root = reactive(input$drive))
+#' }
+#'
+#' shinyApp(ui, server)
+#'
+#' }
+#' @export
+get_drives <- function() {
+  if (Sys.info()["sysname"] == "Windows") {
+    names(which(sapply(paste0(LETTERS, ":/"), dir.exists)))
+  } else {
+    NULL
+  }
+}
